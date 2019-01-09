4b57ce03725148d1c7498995321835339cc7ac2c
Brylie Christopher Oxley <brylie.oxley@maas.global>
2019-01-09 12:02:41 +0200

    Initial Booking endpoints functions
    Signed-off-by: Brylie Christopher Oxley <brylie.oxley@maas.global>

---
+++ api.py
@@ -0,0 +1,23 @@
+def bookings_options_get(message):
+    # do something
+    return 'Message: {}'.format(message), 200
+
+
+def bookings_get(message):
+    # do something
+    return 'Message: {}'.format(message), 200
+
+
+def bookings_post(message):
+    # do something
+    return 'Message: {}'.format(message), 200
+
+
+def bookings_id_get(message):
+    # do something
+    return 'Message: {}'.format(message), 200
+
+
+def bookings_id_put(message):
+    # do something
+    return 'Message: {}'.format(message), 200
