45c5b134a646b66749cc429fd65d0805dfa241fc
Christian MÄHLER (PTV Group) <Christian.Maehler@ptvgroup.com>
2019-04-08 12:32:40 +0200

    test update app.py
    test auf Branch

--- mock_server/app.py
+++ mock_server/app.py
@@ -3,6 +3,6 @@ import connexion
 app = connexion.App(__name__, specification_dir='../specifications/')
 app.add_api('booking.yaml')
 app.run(port=8080)
-
+# test
 if __name__ == "__main__":
     app.run()
