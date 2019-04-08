b42bbbc319148db23237b7b9e0d9e7a5211922fd
Christian MÄHLER (PTV Group) <Christian.Maehler@ptvgroup.com>
2019-04-08 13:38:12 +0200

    Removed base_path because it prevents Swagger UI from working
    Removed base_path because it prevents Swagger UI from working

--- mock_server/app.py
+++ mock_server/app.py
@@ -1,7 +1,7 @@
 import connexion
 
 app = connexion.FlaskApp(__name__, specification_dir='../specifications/')
-app.add_api('booking.yaml', base_path='/booking')
+app.add_api('booking.yaml')
 #app.add_api('maas-api.yaml')
 
 app.run(port=8080)
