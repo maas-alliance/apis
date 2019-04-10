64b3a5bea40ec873675300b067e9ef3ff3a987d4
Christian MÄHLER (PTV Group) <Christian.Maehler@ptvgroup.com>
2019-04-10 11:41:26 +0200

    Added a first mock JSON and integrated ReDoc
    - added a mock for /bookings GET by reading a manually carfted JSON example
    - adapted the "from" parameter to "_from" for /bookings/options/ GET both in the booking.yaml and the handler definition to address a reserved word issue
    - added ReDoc support by adding a staticfile.yaml and the corresponding handler + redoc.htm; added a x-logo section in info: section to booking.yaml

--- mock_server/api.py
+++ mock_server/api.py
@@ -1,4 +1,5 @@
 import json
+import flask
 
 
 # TODO - how to cope with optional parameters?
@@ -6,7 +7,7 @@ import json
 # TODO -    how to map parameters in API to handler parameters?
 # - 
 # changed from to fromP because from is a reserved word in Python
-def bookings_options_get(fromP, to, startTime):
+def bookings_options_get(_from, to, startTime):
     # do something
     print("/bookings/options/ GET")
     return 'Message: {}'.format(to), 200
@@ -18,6 +19,7 @@ def bookings_options_get(fromP, to, startTime):
 def bookings_get(state):
     # do something
     print("/bookings GET - " + state)
+    # get static, manually defined JSON from a given folder
     with open("./mock_json/mock_data_bookings_get.json", "r") as read_file:
         json_string = json.load(read_file)
     #print(json_string)
@@ -37,3 +39,15 @@ def bookings_id_get(message):
 def bookings_id_put(message):
     # do something
     return 'Message: {}'.format(message), 200
+
+
+# #####################################
+# add static part
+
+# deliver a ReDoc based documentation
+# ReDoc can be found here https://github.com/Rebilly/ReDoc
+# MIT licence
+def static_redoc():
+    print("Deliver redoc")
+    return flask.send_file('redoc.htm')
+    
