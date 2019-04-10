d5100a377a02096c8f89605fd6c71ebb8a413e2f
Christian MÄHLER (PTV Group) <Christian.Maehler@ptvgroup.com>
2019-04-10 11:41:26 +0200

    Added a first mock JSON and integrated ReDoc
    - added a mock for /bookings GET by reading a manually carfted JSON example
    - adapted the "from" parameter to "_from" for /bookings/options/ GET both in the booking.yaml and the handler definition to address a reserved word issue
    - added ReDoc support by adding a staticfile.yaml and the corresponding handler + redoc.htm; added a x-logo section in info: section to booking.yaml

--- mock_server/app.py
+++ mock_server/app.py
@@ -1,7 +1,9 @@
 import connexion
+import flask
 
 app = connexion.FlaskApp(__name__, specification_dir='../specifications/')
-app.add_api('booking.yaml')
+app.add_api('booking.yaml', base_path='/booking')
+app.add_api('staticfile.yaml', base_path='/redoc')
 #app.add_api('maas-api.yaml')
 
 app.run(port=8080)
